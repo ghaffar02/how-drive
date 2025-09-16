bd23fc716d814efc3f3826c7b8fdb10253723b7d
faheem4531 <faheem4531@gmail.com>
2025-09-16 19:45:45 +0500

    themes file added , inter font import in global.css

---
+++ src/utils/themes.ts
@@ -0,0 +1,168 @@
+const localFont = {
+  // all headings here
+  h1: {
+    lineHeight: {xs: '58px', md: '100%'},
+    color: '#000',
+    fontSize: {xs: '48px', md: '56px', lg: '64px'},
+    textWrap: {md: 'nowrap'},
+    fontFamily: 'Satoshi700 !important'
+  },
+  h2: {
+    color: '#000',
+    fontSize: {xs: '32px', md: '36px', lg: '48px'},
+    lineHeight: {xs: '1.2em', lg: '1.15em'},
+    fontWeight: 700,
+    fontFamily: 'Satoshi700 !important'
+  },
+  h3: {
+    fontSize: {xs: '28px', sm: '32px', lg: '36px'},
+    lineHeight: {xs: '35px', sm: '100%'},
+    fontWeight: 400,
+    fontFamily: 'Satoshi500 !important',
+    color: '#000'
+  },
+  h4: {},
+  h5: {},
+  h6: {},
+  h7: {},
+  h8: {},
+  h9: {},
+
+  // all paragraphs here
+
+  p1: {
+    fontSize: {xs: '28px', md: '32px', lg: '36px'},
+    color: '#000'
+  },
+  p2: {
+    color: '#1a202c',
+    fontSize: {xs: '20px', sm: '22px', md: '24px'},
+    letterSpacing: '0.03em',
+    lineHeight: '1.3em'
+  },
+  p3: {
+    color: '#000',
+    fontSize: {xs: '18px', md: '20px', lg: '22px'},
+    fontFamily: '"Inter", sans-serif  !important',
+    fontWeight: '300',
+    lineHeight: '1.35em'
+  },
+  p4: {
+    color: '#FFFFFF',
+    fontSize: {xs: '16px', md: '18px', lg: '20px'},
+    lineHeight: '1.35em',
+    fontFamily: '"Inter", sans-serif  !important',
+    fontWeight: '400'
+  },
+  p5: {
+    fontSize: {xs: '14px', sm: '16px', lg: '18px'},
+    lineHeight: {xs: '22px', sm: '100%'},
+    color: '#000000'
+  },
+  p6: {
+    color: '#2d3748',
+    fontSize: {xs: '14px', sm: '15px', md: '16px'},
+    fontFamily: '"Inter", sans-serif !important',
+    letterSpacing: '0.01em',
+    lineHeight: '1.5em'
+  },
+  p7: {
+    fontSize: {xs: '14px !important', sm: '16px !important'}
+  },
+  p8: {
+    color: '#626b7b',
+    fontSize: {xs: '12px', md: '13px', lg: '14px'},
+    fontFamily: '"Inter", sans-serif  !important'
+  },
+  p9: {},
+  p10: {},
+  p11: {},
+  p12: {},
+  p13: {}
+};
+
+export default localFont;
+
+// // import { localFont } from "@/app/utils/themes";
+// const localFont = {
+//   // headings
+//   h1: {
+//     fontSize: {xs: '28px', sm: '32px', lg: '36px'},
+//     lineHeight: {xs: '35px', sm: '100%'},
+//     fontWeight: 400,
+//     fontFamily: 'Satoshi500 !important',
+//     color: '#000'
+//   },
+//   h2: {
+//     lineHeight: {xs: '58px', md: '100%'},
+//     color: '#000',
+//     fontSize: {xs: '48px', md: '56px', lg: '64px'},
+//     textWrap: {md: 'nowrap'},
+//     fontFamily: 'Satoshi700 !important'
+//   },
+//   h3: {
+//     color: '#000',
+//     fontSize: {xs: '32px', md: '36px', lg: '48px'},
+//     lineHeight: {xs: '1.2em', lg: '1.15em'},
+//     fontWeight: 700,
+//     fontFamily: 'Satoshi700 !important'
+//   },
+//   h4: {},
+//   h5: {},
+//   h6: {},
+//   h7: {},
+//   h8: {},
+//   h9: {},
+
+//   // textual
+//   p1: {
+//     color: '#1a202c',
+//     fontSize: {xs: '20px', sm: '22px', md: '24px'},
+//     letterSpacing: '0.03em',
+//     lineHeight: '1.3em'
+//   },
+//   p2: {
+//     color: '#2d3748',
+//     fontSize: {xs: '14px', sm: '15px', md: '16px'},
+//     fontFamily: '"Inter", sans-serif !important',
+//     letterSpacing: '0.01em',
+//     lineHeight: '1.5em'
+//   },
+//   p3: {
+//     color: '#626b7b',
+//     fontSize: {xs: '12px', md: '13px', lg: '14px'},
+//     fontFamily: '"Inter", sans-serif  !important'
+//   },
+//   p4: {
+//     fontSize: {xs: '28px', md: '32px', lg: '36px'},
+//     color: '#000'
+//   },
+//   p5: {
+//     fontSize: {xs: '14px', sm: '16px', lg: '18px'},
+//     lineHeight: {xs: '22px', sm: '100%'},
+//     color: '#000000'
+//   },
+//   p6: {
+//     color: '#000',
+//     fontSize: {xs: '18px', md: '20px', lg: '22px'},
+//     fontFamily: '"Inter", sans-serif  !important',
+//     fontWeight: '300',
+//     lineHeight: '1.35em'
+//   },
+//   p7: {
+//     color: '#FFFFFF',
+//     fontSize: {xs: '16px', md: '18px', lg: '20px'},
+//     lineHeight: '1.35em',
+//     fontFamily: '"Inter", sans-serif  !important',
+//     fontWeight: '400'
+//   },
+//   p8: {
+//     fontSize: {xs: '14px !important', sm: '16px !important'}
+//   },
+//   p9: {},
+//   p10: {},
+//   p11: {},
+//   p12: {},
+//   p13: {}
+// };
+// export default localFont;
