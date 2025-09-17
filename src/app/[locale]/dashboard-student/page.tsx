db2e4d0a57fadcf10405f0179b6f7a7c153e190e
faheem4531 <faheem4531@gmail.com>
2025-09-17 12:50:42 +0500

    StudentDashboard stable version

--- src/app/[locale]/dashboard-student/page.tsx
+++ src/app/[locale]/dashboard-student/page.tsx
@@ -4,7 +4,17 @@ import {Box} from '@mui/material';
 
 export default function StudentDashboard() {
   return (
-    <Box sx={{width: '100%'}}>
+    <Box
+      sx={{
+        backgroundImage: "url('/bgStudentDashboard.svg')",
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%',
+        padding: '24px',
+        backgroundColor: '#fff'
+      }}
+    >
       <StudentLayout />
     </Box>
   );
