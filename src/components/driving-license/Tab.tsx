e7439dc94d85547445849923e466136d5f09412f
faheem4531 <faheem4531@gmail.com>
2025-09-19 16:32:49 +0500

    translate page about us

--- src/components/driving-license/Tab.tsx
+++ src/components/driving-license/Tab.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, {useState} from 'react';
 import {Box, Tabs, Typography, useMediaQuery, useTheme} from '@mui/material';
-import {motion, AnimatePresence} from 'framer-motion';
+import {motion} from 'framer-motion';
 
 type Step = {
   title: string;
