3ba4758bac08be67f3fb2c527d9c727a694e7f57
faheem4531 <faheem4531@gmail.com>
2025-09-19 21:28:52 +0500

    add routes for both pages

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -6,7 +6,61 @@ export const routing = defineRouting({
   pathnames: {
     '/': '/',
     '/login': {
-      de: '/germanlogin'
+      de: '/anmelden'
+    },
+    '/signup': {
+      de: '/registrieren'
+    },
+    '/forgot-password': {
+      de: '/passwort-vergessen'
+    },
+    '/reset-password': {
+      de: '/passwort-zuruecksetzen'
+    },
+    '/about-us': {
+      de: '/ueber-uns'
+    },
+    '/pricing': {
+      de: '/preis'
+    },
+    '/contact': {
+      de: '/kontakt'
+    },
+    '/driving-license-class-a': {
+      de: '/fuehrerscheinklasse-a'
+    },
+    '/driving-license-class-b': {
+      de: '/fuehrerscheinklasse-b'
+    },
+    '/driving-license-class-c': {
+      de: '/fuehrerscheinklasse-c'
+    },
+    '/driving-license-class-d': {
+      de: '/fuehrerscheinklasse-d'
+    },
+    '/driving-license-class-l-and-t': {
+      de: '/fuehrerscheinklasse-l-und-t'
+    },
+    '/rewrite-of-driving-licenses': {
+      de: '/umschreiben-der-fuehrerscheine'
+    },
+    '/help': {
+      de: '/hilfe'
+    },
+    '/imprint': {
+      de: '/impressum'
+    },
+    '/privacy': {
+      de: '/datenschutz'
+    },
+    '/terms': {
+      de: '/agb'
+    },
+    '/cookies': {
+      de: '/cookies'
+    },
+    '/404': {
+      de: '/404'
     }
   }
 });
