9cc939af02ec322d8a7b01de364664f34b0849f9
faheem4531 <faheem4531@gmail.com>
2025-09-30 15:01:41 +0500

    fix issue in account section

--- src/components/student-dashboard/InputField.tsx
+++ src/components/student-dashboard/InputField.tsx
@@ -11,6 +11,7 @@ interface CustomTextFieldProps {
 
 export default function CustomTextField({
   bgColor = '#f8fafcff',
+  // bgColor = '#fff',
   borderColor = '#E2E8F0',
   hoverBorderColor = '#1976d2',
   radius = '8px',
@@ -20,24 +21,28 @@ export default function CustomTextField({
 }: CustomTextFieldProps) {
   return (
     <TextField
+      placeholder={labal}
       {...rest}
       sx={{
-        maxWidth: '400px',
+        // maxWidth: '400px',
         width: '100%',
 
         '& .MuiOutlinedInput-root': {
           background: bgColor,
           borderRadius: radius,
+
           boxShadow:
             '0px 0px 0px 1px rgba(0, 0, 0, 0.05), 0px 1px 0px 0px rgba(0, 0, 0, 0.05), 0px 2px 4px 0px rgba(0, 0, 0, 0.08)',
           // border: `1px solid ${borderColor}`,
           '& .MuiOutlinedInput-input': {
             padding: '12px',
-            fontSize: '12px',
+            fontSize: '14px',
             fontFamily: '"Inter", sans-serif !important',
-            color: '#000',
+            lineHeight: '1px !important',
+            // color: '#eae7e7ff',
             '&::placeholder': {
-              color: '#999999',
+              color: '#999',
+              // lineHeight: '16px !important',
               opacity: 1
             }
           },
@@ -51,7 +56,7 @@ export default function CustomTextField({
         ...sx
       }}
     >
-      {labal}
+      {labal}dkjhkuqiuqi
     </TextField>
   );
 }
--- src/components/student-dashboard/layout/profile-setting/components/Account.tsx
+++ src/components/student-dashboard/layout/profile-setting/components/Account.tsx
@@ -4,11 +4,38 @@ import localFont from '@/utils/themes';
 import {Box, Divider, Typography} from '@mui/material';
 
 export default function Account() {
+  const formFields = [
+    {
+      label: 'First name',
+      placeholder: 'Daniel'
+    },
+    {
+      label: 'Last name',
+      placeholder: 'Mustermann'
+    },
+    {
+      label: 'Email',
+      placeholder: 'd.mustermann@gmail.com'
+    },
+    {
+      label: 'Place of residence',
+      placeholder: 'Hamburg'
+    },
+    {
+      label: 'Phone number',
+      placeholder: '01518745692'
+    },
+    {
+      label: 'Driving school',
+      placeholder: 'Mundsburg'
+    }
+  ];
+
   return (
     <Box
       sx={{
         width: '100%',
-        height: '100%',
+       
         mt: '12px',
         display: 'flex',
         flexDirection: 'column',
@@ -60,7 +87,7 @@ export default function Account() {
             label="Abbrechen"
             bgColor="rgba(248, 250, 252, 0)"
             hoverTextcolor="#fff"
-            sx={{border: '1px solid #000', color: '#000'}}
+            sx={{border: '1px solid #a1a1aaff', color: '#000'}}
           />
           <CustomButton label="save" sx={{}} />
         </Box>
@@ -79,9 +106,10 @@ export default function Account() {
       <Box sx={{width: '100%'}}>
         <Typography
           sx={{
-            ...localFont.inter18,
+            ...localFont.inter16,
             fontWeight: 500,
-            fontFamily: '"Inter", sans-serif !important'
+            fontFamily: '"Inter", sans-serif !important',
+            mb: '32px'
           }}
         >
           Konto
@@ -90,22 +118,116 @@ export default function Account() {
           sx={{
             width: '100%',
             display: 'flex',
-            flexDirection: {xs: 'column', md: 'row'},
-            gap: '8px',
+            flexDirection: {xs: 'column'},
+            gap: '20px',
             alignItems: 'start',
             justifyContent: 'space-between'
           }}
         >
+          {formFields.map((items, i) => {
+            return (
+              <Box
+                key={i}
+                sx={{
+                  width: '100%',
+                  display: 'flex',
+                  flexDirection: {xs: 'column', lg: 'row'},
+                  gap: {xs: '8px', lg: '0px'},
+                  alignItems: 'start',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <Typography
+                  sx={{
+                    ...localFont.inter14,
+                    width: '100%',
+                    maxWidth: '405px',
+                    fontFamily: '"Inter", sans-serif !important',
+                    fontWeight: 400,
+                    textAlign: 'left'
+                    // mt: {xs: '8px', lg: '0px'}
+                  }}
+                >
+                  {items.label}
+                </Typography>
+                <CustomTextField
+                  labal={items.placeholder}
+                  sx={{textAlign: 'end'}}
+                />
+              </Box>
+            );
+          })}
+        </Box>
+      </Box>
+      <Box width="100%">
+        <Divider
+          sx={{
+            mt: '32px',
+            borderTop: '1px solid transparent',
+            borderImage:
+              'linear-gradient(90deg, rgba(245,245,245,0.6) 0%, rgba(203,203,203,1) 50%, rgba(245,245,245,0.6) 100%) 1'
+            // marginBottom: '32px'
+          }}
+        />
+      </Box>
+      <Box
+        sx={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: {xs: 'column', lg: 'row'},
+          // flexWrap: 'wrap',
+          gap: '16px',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+          // mb: '12px'
+        }}
+      >
+        <Box sx={{width: '100%', maxWidth: {lg: '550px'}}}>
           <Typography
             sx={{
               ...localFont.inter16,
+              fontWeight: 500,
+              fontFamily: '"Inter", sans-serif !important'
+            }}
+          >
+            Delete account
+          </Typography>
+          <Typography
+            sx={{
+              width: '100%',
+              ...localFont.inter14,
               fontFamily: '"Inter", sans-serif !important',
               fontWeight: 300
             }}
           >
-            Persönliche
+            If you delete your account, you will lose access to your data and
+            the driver&apos;s license process. We will permanently delete your
+            personal data.
           </Typography>
-          <CustomTextField labal="Deniel" />
+        </Box>
+        <Box
+          sx={{
+            width: '100%',
+            p: '4px',
+            display: 'flex',
+            gap: '10px',
+            alignItems: 'center',
+            justifyContent: 'flex-end'
+            // justifyContent: 'space-between'
+          }}
+        >
+          <CustomButton
+            label="Delete account"
+            bgColor="rgb(220, 38, 38)"
+            hoverColor="#991919"
+            sx={{}}
+          />
+          <CustomButton
+            label="learn more"
+            bgColor="rgba(248, 250, 252, 0)"
+            hoverTextcolor="#fff"
+            sx={{border: '1px solid #a1a1aaff', color: '#000'}}
+          />
         </Box>
       </Box>
     </Box>
