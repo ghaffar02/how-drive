b4237852592962531fb0f2ff217a89347cff26b9
faheem4531 <faheem4531@gmail.com>
2025-10-05 18:39:56 +0500

    calander updates

--- src/components/student-dashboard/layout/calander/components/MiniFramerCalendar.tsx
+++ src/components/student-dashboard/layout/calander/components/MiniFramerCalendar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import {DayPicker} from 'react-day-picker';
-import {de} from 'date-fns/locale';
+import {enGB} from 'date-fns/locale';
 import 'react-day-picker/style.css';
 import styles from './mini-framer-calendar.module.css';
 
@@ -13,25 +13,25 @@ export default function MiniFramerCalendar() {
 
   // Grey “badge” days like in your screenshot
   const eventDays = [
-    new Date(2025, 9, 13),
-    new Date(2025, 9, 14),
-    new Date(2025, 9, 16),
-    new Date(2025, 9, 17),
-    new Date(2025, 9, 20),
-    new Date(2025, 9, 21),
-    new Date(2025, 9, 23),
-    new Date(2025, 9, 24),
-    new Date(2025, 9, 27),
-    new Date(2025, 9, 28),
-    new Date(2025, 9, 29),
-    new Date(2025, 9, 30),
-    new Date(2025, 9, 31)
+    // new Date(2025, 9, 13),
+    // new Date(2025, 9, 14),
+    // new Date(2025, 9, 16),
+    // new Date(2025, 9, 17),
+    new Date(2025, 9, 20)
+    // new Date(2025, 9, 21),
+    // new Date(2025, 9, 23),
+    // new Date(2025, 9, 24),
+    // new Date(2025, 9, 27),
+    // new Date(2025, 9, 28),
+    // new Date(2025, 9, 29),
+    // new Date(2025, 9, 30),
+    // new Date(2025, 9, 31)
   ];
 
   return (
     <div className={styles.wrap}>
       <DayPicker
-        locale={de}
+        locale={enGB}
         mode="single"
         selected={selected}
         onSelect={setSelected}
