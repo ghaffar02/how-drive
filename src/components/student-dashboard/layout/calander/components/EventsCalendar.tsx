25d9bc9f00a22ed4a209e98d445b0a8cc6e54036
faheem4531 <faheem4531@gmail.com>
2025-10-15 19:45:51 +0500

    calander stable version

--- src/components/student-dashboard/layout/calander/components/EventsCalendar.tsx
+++ src/components/student-dashboard/layout/calander/components/EventsCalendar.tsx
@@ -6,6 +6,7 @@ import calander from '@/assets/svgs/dashboard-student/calander/calander.svg';
 import plus from '@/assets/svgs/dashboard-student/calander/plus.svg';
 import * as React from 'react';
 import localFont from '@/utils/themes';
+import BigCalendar from './big-calander/BigCalander';
 
 export default function EventsCalendar() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -30,17 +31,19 @@ export default function EventsCalendar() {
         boxShadow:
           'rgb(255, 255, 255) 0px 0px 0px 1px, rgba(0, 0, 0, 0.25) 0px 1px 0px 0px, rgba(0, 0, 0, 0.25) 0px 1px 1px 0px',
         backdropFilter: 'blur(15px)',
-        borderRadius: {xs: '24px', md: '0px 24px 24px 0px'},
-        height: '100%'
+        borderRadius: {xs: '24px', md: '0px 24px 24px 0px'}
+        // height: '100%'
       }}
     >
+      {/* ther header there  */}
       <Box
         sx={{
           overflowY: 'scroll',
-          height: '100%',
+          // height: '100%',
           overflow: 'hidden auto',
           msOverflowStyle: 'none',
-          scrollbarWidth: 'none'
+          scrollbarWidth: 'none',
+          paddingBottom: '24px'
         }}
       >
         <Box
@@ -79,9 +82,12 @@ export default function EventsCalendar() {
           </Box>
         </Box>
       </Box>
-
       {/* Anchored Modal */}
       <EventAddPopover anchorEl={anchorEl} open={open} onClose={handleClose} />
+      {/* the big calander there  */}
+      {/* <Box> */}
+      <BigCalendar />
+      {/* </Box> */}
     </Box>
   );
 }
---
+++ src/components/student-dashboard/layout/calander/components/big-calander/BigCalander.tsx
@@ -0,0 +1,88 @@
+'use client';
+import React from 'react';
+import {Calendar, dateFnsLocalizer} from 'react-big-calendar';
+import {format, parse, startOfWeek, getDay} from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import styles from './BigCalander.module.css';
+
+const locales = {'en-US': enUS};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+});
+
+const events = [
+  {
+    title: 'Event 5',
+    start: new Date(2025, 9, 9),
+    end: new Date(2025, 9, 9),
+    color: '#4F46E5'
+  },
+  {
+    title: 'Event 12',
+    start: new Date(2025, 9, 13),
+    end: new Date(2025, 9, 13),
+    color: '#A855F7'
+  },
+  {
+    title: 'Event 3',
+    start: new Date(2025, 9, 13),
+    end: new Date(2025, 9, 13),
+    color: '#06B6D4'
+  },
+  {
+    title: 'Event 5',
+    start: new Date(2025, 9, 17),
+    end: new Date(2025, 9, 17),
+    color: '#4F46E5'
+  },
+  {
+    title: 'Event 12',
+    start: new Date(2025, 9, 17),
+    end: new Date(2025, 9, 17),
+    color: '#A855F7'
+  },
+  {
+    title: 'Event 3',
+    start: new Date(2025, 9, 31),
+    end: new Date(2025, 9, 31),
+    color: '#06B6D4'
+  }
+];
+
+const EventComponent: React.FC<{event: any}> = ({event}) => (
+  <div
+    style={{
+      width: '5px',
+      height: '12px',
+      borderRadius: '3px',
+      margin: '1px auto',
+      backgroundColor: event.color
+    }}
+  />
+);
+
+export default function BigCalendar() {
+  return (
+    <div className={styles.calendarWrapper}>
+      <div className={styles.calendarContainer}>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          components={{event: EventComponent}}
+          views={['month']}
+          toolbar
+          popup={false}
+          selectable={false}
+          style={{flex: 1, height: '100%', width: '100%'}}
+        />
+      </div>
+    </div>
+  );
+}
