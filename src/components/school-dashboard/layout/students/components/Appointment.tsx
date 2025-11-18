acb89f5f9d9e93c648c7110aef0daa70cedeab42
faheem4531 <faheem4531@gmail.com>
2025-11-18 17:56:54 +0500

    appointment height fixed

--- src/components/school-dashboard/layout/students/components/Appointment.tsx
+++ src/components/school-dashboard/layout/students/components/Appointment.tsx
@@ -178,7 +178,7 @@ export default function Appointment() {
           `,
                   borderRadius: '12px',
                   transformOrigin: 'top right',
-                  height: '80vh',
+                  height: '500px',
                   scrollbarWidth: 'none',
                   msOverflowStyle: 'none',
                   '&::-webkit-scrollbar': {
