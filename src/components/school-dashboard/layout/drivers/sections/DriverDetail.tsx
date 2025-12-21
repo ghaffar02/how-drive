455a310ad8ba97dd5d107e57584623565fe6132a
Marsad Ghanvi <marsad11223@gmail.com>
2025-12-21 23:05:09 +0500

    calander issues fixed

--- src/components/school-dashboard/layout/drivers/sections/DriverDetail.tsx
+++ src/components/school-dashboard/layout/drivers/sections/DriverDetail.tsx
@@ -115,8 +115,8 @@ export default function DriverDetail({activeKey}: DriverDetailProps) {
               '& > :first-child': {
                 height: {xs: '640px', md: '650px', lg: '866px'}
               },
-              padding: {xs: '8px', md: '24px'},
-              backgroundColor: 'rgba(248, 250, 252, 0.6)',
+              // padding: {xs: '8px', md: '24px'},
+              // backgroundColor: 'rgba(248, 250, 252, 0.6)',
               borderRadius: '24px',
               border: '2px solid #fff'
             }}
